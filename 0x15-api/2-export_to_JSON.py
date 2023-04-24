9cc0afb314935da0aedd36d566e5303fd84ade43
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-04-24 10:25:30 -0700

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""DataAPI module"""
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    api_url = 'https://jsonplaceholder.typicode.com'
+    employee_id = sys.argv[1]
+    resp1 = requests.get('{}/users/{}'.format(api_url, employee_id))
+    employee_username = resp1.json().get('username')
+    resp2 = requests.get('{}/todos?userId={}'.format(api_url, employee_id))
+    responses = resp2.json()
+    with open('{}.json'.format(employee_id), 'w') as file_:
+        tasks_list = []
+        data = {employee_id: tasks_list}
+        for resp in responses:
+            tasks_list.append({
+                "task": resp.get('title'),
+                "completed": resp.get('completed'),
+                "username": employee_username
+            })
+        json.dump(data, file_)
