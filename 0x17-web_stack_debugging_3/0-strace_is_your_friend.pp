b298082ec1f861c4b6e13d47cf68ac5b726dd95e
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-05-10 08:10:06 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Web stack debugging 3
 exec { 'debug wordpress':
-    environment => ['Dir=/var/www/html/wp-settings.php',
+    environment => ['DIR=/var/www/html/wp-settings.php',
         'OLD_PHPP=phpp', 'NEW_PHP=php'],
     command     => 'sudo sed -i "s/$OLD_PHPP/$NEW_PHP/" $DIR',
     path        => ['/usr/bin', '/bin'],
