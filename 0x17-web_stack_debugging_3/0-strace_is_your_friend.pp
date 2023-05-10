69f72d05b43e951990488c0e2390ff45c3a0a5cf
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-05-10 06:59:19 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 file {'/var/www/html/index.html':
   ensure  => file,
-  mode    => '0666',
+  mode    => '0644',
   content => '<title>Holberton &#8211; Just another WordPress site</title>
 <link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Feed" href="http://127.0.0.1/?feed=rss2" />
 <link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
