7ebcbb357fa4f8338fa6c79646b5dff4b7f35fcd
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-05-09 18:57:46 -0700

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Api advanced """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Return number of subscribers """
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    Apiurl = 'https://www.reddit.com/r/{subreddit}/about.json'.format(subreddit)
+    resp = requests.get(Apiurl, headers=headers, allow_redirects=False)
+    if resp.status_code == 200:
+        data = resp.json()
+        return data['data']['subscribers']
+    else:
+        return 0
