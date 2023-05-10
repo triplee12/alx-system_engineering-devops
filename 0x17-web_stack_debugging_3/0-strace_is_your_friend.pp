b87d02f9e070b577917035cd8996f1ae54821df3
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-05-10 06:53:15 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,10 @@
-file {'var/www/html/index.html';
+file {'var/www/html/index.html':
   ensure  => file,
   mode    => '0666',
-  content => '<html><head><title>Web Debugging #3</title></head><body><h1>Hello, World!</h1></body></html>'
+  content => '<title>Holberton &#8211; Just another WordPress site</title>
+<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Feed" href="http://127.0.0.1/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
+        <div id="wp-custom-header" class="wp-custom-header"><img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Holberton" /></div>  </div>
+                            <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
+        <p>Yet another bug by a Holberton student</p>'
 }
