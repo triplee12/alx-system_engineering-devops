93ab3f6e4ec1667cdaa681bd68dd7ceda78c59a0
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-05-09 19:44:15 -0700

    Return reddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ Return number of subscribers """
     headers = {'User-Agent': 'Mozilla/5.0'}
-    Apiurl = 'https://www.reddit.com/r/{subreddit}/about.json'.format(subreddit)
+    Apiurl = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     resp = requests.get(Apiurl, headers=headers, allow_redirects=False)
     if resp.status_code == 200:
         data = resp.json()
