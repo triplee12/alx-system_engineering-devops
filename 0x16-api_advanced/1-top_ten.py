4358a8e8e3ad17c662fb4a57c09d0373343811cb
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-05-09 19:47:42 -0700

    Top 10 posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """ Return top 10 posts """
     headers = {'User-Agent': 'Mozilla/5.0'}
-    Apiurl = 'https://www.reddit.com/r/{subreddit}/hot.json'.format(subreddit)
+    Apiurl = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     resp = requests.get(Apiurl, headers=headers, allow_redirects=False)
     if resp.status_code == 200:
         data = resp.json()
